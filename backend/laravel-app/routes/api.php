bcfe7785225c073a60bec7f6dc1b5f481f5c77c3
raghadshaar <raghadshaar8@gmail.com>
2026-04-12 18:24:13 +0300

    backend for caption genertaing feature

--- backend/laravel-app/routes/api.php
+++ backend/laravel-app/routes/api.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CaptionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/image-edit', [App\Http\Controllers\ImageEditController::class, 'edit']);
+Route::post('/captions/generate', [CaptionController::class, 'generate']);
