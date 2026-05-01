73e579f1de2243cd415a3252d878a2421a48856f
raghadshaar <raghadshaar8@gmail.com>
2026-05-01 12:22:49 +0300

    link subscription with captions

--- backend/laravel-app/app/Http/Controllers/Api/CaptionController.php
+++ backend/laravel-app/app/Http/Controllers/Api/CaptionController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\Services\UsageLimitService;
 use Illuminate\Validation\ValidationException;
-
+//
 //////
 class CaptionController extends Controller
 {
